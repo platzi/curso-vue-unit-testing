2ff3d4d0d55606c1438c1b4fccbebb7324e3b3e0
camilaleniss <mlmariacami2@gmail.com>
2022-06-13 11:26:23 -0500

    Add test for a component with dependencies

--- qr-generator-vue-3/tests/unit/components/qr_code_input.spec.js
+++ qr-generator-vue-3/tests/unit/components/qr_code_input.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 
 import QRCodeInput from '@/components/QRCodeInput.vue'
+import { createStore } from 'vuex'
 
 describe('QRCodeInput.vue', () => {
   describe('mouting a component', () => {
@@ -10,6 +11,26 @@ describe('QRCodeInput.vue', () => {
       const component = wrapper.find('.hello')
 
       expect(component.classes()).toContain('hello')
+    })
+  })
+  describe('mounting a component with its dependencies', () => {
+    const store = createStore({
+      state () {
+        return { count: 1 }
+      }
+    })
+
+    const wrapper = shallowMount(QRCodeInput,
+      {
+        global: {
+          plugins: [store]
+        }
+      })
+
+    it('renders generate qr code button with text', () => {
+      const component = wrapper.find('#btn-generate')
+
+      expect(component.text()).toBe('Generar QR')
     })
   })
 })
